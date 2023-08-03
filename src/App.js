fca18cb42e099b0bede4cf78ed0d648df21b04c0
Kostya <umsha.kostya@webmaestro.com.ua>
2023-08-03 17:19:35 +0300

    added sort template

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import '../src/styles/App.scss';
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import MySelect from "./components/ui/MySelect";
 
 function App() {
         const [tasks, setTasks] = useState([
@@ -10,18 +11,33 @@ function App() {
             {id: 3, userId: 2, title: '1231rq', completed: true},
             {id: 4, userId: 3, title: 'dasfsdfsfat', completed: false}
         ])
-
+    const [selectedSort, setSelectedSort] = useState('')
     const createTask = (newTask) => {
             setTasks([...tasks, newTask])
     }
     const removeTask = (task) => {
             setTasks(tasks.filter(t => t.id !== task.id))
     }
+    const sortPosts = (sort) => {
+
+    }
 
 
   return (
     <div className="app container">
         <TaskForm tasks={tasks} create={createTask}/>
+        <MySelect
+            value={selectedSort}
+            onChange={sortPosts}
+            defaultValue="Sort:"
+            options={[
+            {value: 'titleName', name:'By task name'},
+            {value: 'userIdAsc', name:'By user 0-9'},
+            {value: 'userIdDesc', name:'By user 9-0'},
+            {value: 'completed', name:'Completed'},
+            {value: 'uncompleted', name:'Not completed'},
+
+        ]}/>
         {tasks.length !== 0
         ? <TaskList tasks={tasks} title='Tasks' remove={removeTask}/>
         : <span className={'app__not-found'}>Tasks not found!</span>
